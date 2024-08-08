6442584415e18e5a698553d66e13853c4f66b91e
Aniri <bububuiok@mail.ru>
2024-08-08 14:34:47 +0500

    Added Volunteer model

---
+++ backend/src/PetHelpers.Domain/Models/Volunteer.cs
@@ -0,0 +1,33 @@
+﻿namespace PetHelpers.Domain.Models;
+
+public class Volunteer
+{
+    public Guid Id { get; } 
+    
+    public string FullName { get; }
+    
+    public string Description { get; }
+    
+    public string PhoneNumber { get; }
+
+    public int YearsOfExperience { get; }
+    
+    public int PetsFoundHome { get; }
+    
+    public int PetsLookingForHome { get; }
+    
+    public int PetsInTreatment { get; }
+
+    public List<SocialMedia> SocialMedias { get; }
+    
+    public List<Requisite> Requisites  { get; }
+
+    public List<Pet> OwnedPets { get; }
+}
+
+public class SocialMedia
+{
+    public string Title { get; }
+    
+    public string Link { get; }
+}
